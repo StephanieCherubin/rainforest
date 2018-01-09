d9cfe45e23bfec1b944e21f211826b1e403e6efa
Stephanie <seraphin.cherubin@gmail.com>
2018-01-09 18:29:35 -0500

    Changed db to PG

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Stephanie", email:"stephanie@cherubin.com", password_digest:"f1f2f3")
+User.create(name: "Jorge", email:"jorge@romao.com", password_digest:"j1j1j1")
+User.create(name: "Will", email:"william@wi.com", password_digest:"kdkd")
+User.create(name: "Mary", email:"mar@google.com", password_digest:"siekgs")
+User.create(name: "Hannah", email:"ha@cherubin.com", password_digest:"3955")
+User.create(name: "Auston", email:"aust@text.com", password_digest:"4w464")
+User.create(name: "PK", email:"patricio@kelly.com", password_digest:"495")
